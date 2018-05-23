1fe395487d3f183934e90148f4315cf99b8e56e1
vladbenchea <vlad.benchea@hpe.com>
2018-05-23 13:54:48 +0300

    indentation

--- octane-eclipse-plugin/src/com/hpe/octane/ideplugins/eclipse/ui/util/error/ErrorComposite.java
+++ octane-eclipse-plugin/src/com/hpe/octane/ideplugins/eclipse/ui/util/error/ErrorComposite.java
@@ -20,7 +20,6 @@ import org.apache.commons.lang.StringUtils;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -38,7 +37,7 @@ import com.hpe.octane.ideplugins.eclipse.ui.util.TruncatingStyledText;
 import com.hpe.octane.ideplugins.eclipse.ui.util.resource.SWTResourceManager;
 
 public class ErrorComposite extends Composite {
-    
+
     public static final String ERORR_MODEL_FIELD_DESCRIPTION_TRANSLATED = "description_translated";
     public static final String ERORR_MODEL_FIELD_DESCRIPTION = "description";
     public static final String ERORR_MODEL_FIELD_PROPERTIES = "properties";
@@ -46,39 +45,40 @@ public class ErrorComposite extends Composite {
     public static final String ERORR_MODEL_FIELD_HTTP_STATUS_CODE = "httpStatusCode";
     public static final String ERORR_MODEL_FIELD_CORRELATION_ID = "correlation_id";
     public static final String ERROR_MODEL_FIELD_STACK_TRACE = "stack_trace";
-    
+
     private Composite compositeExceptionData;
     private Composite compositeBtns;
     private Label lblSeparator;
 
     /**
      * Create the composite.
+     * 
      * @param parent
      * @param style
      */
     public ErrorComposite(Composite parent, int style) {
         super(parent, style);
         setLayout(new GridLayout(1, false));
-        
+
         setBackground(SWTResourceManager.getColor(SWT.COLOR_LIST_BACKGROUND));
         setForeground(SWTResourceManager.getColor(SWT.COLOR_LIST_FOREGROUND));
         setBackgroundMode(SWT.INHERIT_FORCE);
-        
+
         compositeExceptionData = new Composite(this, SWT.NONE);
         compositeExceptionData.setLayout(new GridLayout(1, false));
         compositeExceptionData.setLayoutData(new GridData(SWT.CENTER, SWT.FILL, true, false, 1, 1));
-        
+
         lblSeparator = new Label(this, SWT.SEPARATOR | SWT.HORIZONTAL);
-        lblSeparator.setLayoutData(new GridData(SWT.FILL, SWT.BOTTOM, true, true, 1, 1)); 
+        lblSeparator.setLayoutData(new GridData(SWT.FILL, SWT.BOTTOM, true, true, 1, 1));
         lblSeparator.setVisible(false);
-        
+
         compositeBtns = new Composite(this, SWT.NONE);
         FillLayout fl_compositeBtns = new FillLayout(SWT.HORIZONTAL);
         fl_compositeBtns.spacing = 10;
         compositeBtns.setLayout(fl_compositeBtns);
         compositeBtns.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1));
-    } 
-    
+    }
+
     public void addButton(String btnText, Runnable clickedRunnable) {
         lblSeparator.setVisible(true);
         Button btn = new Button(compositeBtns, SWT.NONE);
@@ -89,59 +89,59 @@ public class ErrorComposite extends Composite {
                 clickedRunnable.run();
             }
         });
-        
+
         setSize(computeSize(SWT.DEFAULT, SWT.DEFAULT));
         layout(true);
     }
-    
+
     public void clearButtons() {
         lblSeparator.setVisible(false);
         Arrays.stream(compositeBtns.getChildren()).forEach(Control::dispose);
     }
-    
+
     public void displayException(Exception ex) {
-        if(ex instanceof OctaneException) {
-            displayOctaneException((OctaneException)ex);
+        if (ex instanceof OctaneException) {
+            displayOctaneException((OctaneException) ex);
         } else {
-            displayGenericException(ex); 
+            displayGenericException(ex);
         }
     }
-    
-    private void displayGenericException(Exception ex) {        
+
+    private void displayGenericException(Exception ex) {
         compositeExceptionData.setLayout(new GridLayout(1, false));
-        
+
         TruncatingStyledText txtErrorField = new TruncatingStyledText(compositeExceptionData, SWT.MULTI);
         txtErrorField.setForeground(SWTResourceManager.getColor(SWT.COLOR_DARK_RED));
         txtErrorField.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, true, 1, 1));
-        
+
         txtErrorField.setText(ex.getMessage());
     }
-    
+
     private void displayOctaneException(OctaneException ex) {
         compositeExceptionData.setLayout(new GridLayout(2, false));
-        
+
         ErrorModel errorModel = ex.getError();
         String description = getDescriptionFromOctaneException(errorModel);
-        
-        if(description != null) {
-            TruncatingStyledText txtErrorField = new TruncatingStyledText(compositeExceptionData, SWT.WRAP| SWT.MULTI | SWT.READ_ONLY);
+
+        if (description != null) {
+            TruncatingStyledText txtErrorField = new TruncatingStyledText(compositeExceptionData, SWT.WRAP | SWT.MULTI | SWT.READ_ONLY);
             txtErrorField.setForeground(SWTResourceManager.getColor(SWT.COLOR_DARK_RED));
             txtErrorField.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
             txtErrorField.setText(description);
-            
+
             errorModel.removeValue(ERORR_MODEL_FIELD_DESCRIPTION);
             errorModel.removeValue(ERORR_MODEL_FIELD_DESCRIPTION_TRANSLATED);
         }
-        
-        //Rest of values displayed generically
+
+        // Rest of values displayed generically
         ex.getError().getValues().forEach(fieldModel -> {
-            
+
             String fieldValueTxt = getFieldModelValueIfPresentAndNotEmpty(errorModel, fieldModel.getName());
-            
-            if(fieldValueTxt == null) {
+
+            if (fieldValueTxt == null) {
                 return;
             }
-            
+
             Label lblErrorField = new Label(compositeExceptionData, SWT.NONE);
             lblErrorField.setText(convertFieldNameToLabel(fieldModel.getName()));
             if (fieldModel.getName().equals(ERROR_MODEL_FIELD_STACK_TRACE)) {
@@ -159,29 +159,29 @@ public class ErrorComposite extends Composite {
             }
         });
     }
-    
+
     private static String getFieldModelValueIfPresentAndNotEmpty(ErrorModel errorModel, String fieldName) {
-        if(errorModel.getValue(fieldName) == null) {
+        if (errorModel.getValue(fieldName) == null) {
             return null;
         }
-        
+
         @SuppressWarnings("rawtypes")
         FieldModel fieldModel = errorModel.getValue(fieldName);
         String value = Util.getUiDataFromModel(fieldModel);
         return value.isEmpty() ? null : value;
     }
-    
+
     public static String getDescriptionFromOctaneException(ErrorModel errorModel) {
         String description = getFieldModelValueIfPresentAndNotEmpty(errorModel, ERORR_MODEL_FIELD_DESCRIPTION);
-        
-        //If null try the alternative
-        if(description == null) {
+
+        // If null try the alternative
+        if (description == null) {
             description = getFieldModelValueIfPresentAndNotEmpty(errorModel, ERORR_MODEL_FIELD_DESCRIPTION_TRANSLATED);
         }
-        
+
         return description;
     }
-    
+
     private static String convertFieldNameToLabel(String fieldname) {
         fieldname = fieldname.replace("_", " ");
         fieldname = Arrays.stream(StringUtils.splitByCharacterTypeCamelCase(fieldname)).collect(Collectors.joining(" "));
@@ -191,5 +191,5 @@ public class ErrorComposite extends Composite {
         fieldname = fieldname + ": ";
         return fieldname;
     }
-    
+
 }
